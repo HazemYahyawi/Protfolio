6e671a1b67a46b262b1fedfd0431f43d27536e71
hazem <hazem.yahyawi159@gmail.com>
2023-05-17 18:02:20 +0200

    Second commit

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -20,7 +20,7 @@ export const Banner = () => {
     }, delta);
 
     return () => { clearInterval(ticker) };
-  }, [delta]);
+  }, [text]);
   const tick = () => {
     let i = loopNum % toRotate.length;
     let fullText = toRotate[i];
